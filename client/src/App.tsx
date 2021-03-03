ece325c0d90a77fa9d33f0b7c52cb35085fcc4a2
Dvir Bartov <dvir.bartov1@gmail.com>
2021-03-03 14:15:12 +0200

    Add client-side, basic style and Topbar component

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style/App.css';
+import Topbar from './components/Topbar/Topbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Topbar/Topbar.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Topbar.css';
+
+export default class Topbar extends Component {
+    render() {
+        return (
+            <div className="topbar">
+                עורך סימולציות פסיכומטרי
+            </div>
+        )
+    }
+}
